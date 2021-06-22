bd18ebb28b8a5ea6cf4f6fa812e8c02699d234a3
Steven Soekha <steven.soekha@outlook.com>
2021-06-22 16:26:06 +0200

    Update query.ts

--- src/core/query.ts
+++ src/core/query.ts
@@ -1,15 +1,14 @@
 import { State } from '../lib/state'
 import { Unit } from '../lib/unit'
 import { SelectedQuery } from './selected-query'
-import { omitMany } from '../utils/omit'
-import { pickMany } from '../utils/pick'
+import { omitMany, pickMany, NonIterable } from '../utils'
 
 export type Query<a> = {
-    select: <k extends keyof a>(...properties: k[]) => SelectedQuery<Omit<a, k>, Pick<a, k>>
+    select: <k extends NonIterable<a>>(...properties: k[]) => SelectedQuery<Omit<a, k>, Pick<a, k>>
 }
 
 export const Query = <a>(state: State<a, Unit>): Query<a> => ({
-    select: function <k extends keyof a>(...properties: k[]): SelectedQuery<Omit<a, k>, Pick<a, k>> {
+    select: function <k extends NonIterable<a>>(...properties: k[]): SelectedQuery<Omit<a, k>, Pick<a, k>> {
         return SelectedQuery(
             state.map(
                 (selectable) => selectable.map((x) => omitMany(x, properties)),
