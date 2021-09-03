0beb2e8043ceff4d553b9514f2f38aa55214cb22
Steven Soekha <steven.soekha@outlook.com>
2021-09-03 11:45:59 +0200

    Add more data

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,47 +2,89 @@ import { Student } from '../models/student'
 import { Grade } from '../models/grade'
 import { Professor } from '../models/professor'
 import { School } from '../models/school'
+import { name, internet, datatype, address } from 'faker'
+import { randint } from '../utils/random'
 
-export const initialize = (): Array<Student> => [
-    Student('Steven', 'Soekha', 24, 'steven.soekha@outlook.com', grades1(), schools1()),
-    Student('Jack', 'Mitzer', 26, 'jack.mitzer@gmail.com', [], schools3()),
-    Student('Ahmed', 'Rashid', 24, 'ahmed.rizgar.rashid@gmail.com', grades2(), schools3()),
-    Student('Liza', 'Smiths', 21, 'liza.smiths@outlook.com', grades2()),
-    Student('Mika', 'Moka', 28, 'mika.moka@gmail.com', [], schools2()),
-    Student('Ava', 'Blue', 23, 'ava.blue@gmail.com', grades2(), schools3()),
-]
+const newStudent = (): Student => {
+    const firstName = name.firstName()
+    const lastName = name.lastName()
+    const email = internet.email(firstName, lastName)
+    const age = datatype.number(100)
+    return Student(firstName, lastName, age, email, newGrade(randint(1, 5)), newSchool(randint(1, 2)))
+}
 
-const grades1 = (): Array<Grade> => [
-    Grade('Development 1', 8.2, 4, professors1()),
-    Grade('Development 2', 7.5, 4, professors3()),
-    Grade('Development 3', 6.0, 4, professors3()),
-    Grade('Development 4', 4.8, 4, professors1()),
-]
+const newProfessor = (): Professor => {
+    const firstName = name.firstName()
+    const lastName = name.lastName()
+    const email = internet.email(firstName, lastName)
+    const age = randint(20, 70)
+    return Professor(firstName, age, email, newSchool(randint(1, 2)))
+}
 
-const grades2 = (): Array<Grade> => [Grade('Development 1', 6.6, 4, professors2())]
+const newSchool = (amount: number, arr: Array<School> = []): Array<School> =>
+    arr.length === amount
+        ? arr
+        : newSchool(amount, [
+              ...arr,
+              School(schools.name[randint(0, schools.name.length)], schools.location(), schools.founded()),
+          ])
 
-const grades3 = (): Array<Grade> => [
-    Grade('Development 1', 8.5, 4, professors3()),
-    Grade('Development 2', 8.8, 4, professors2()),
-]
+const newGrade = (amount: number, arr: Array<Grade> = []): Array<Grade> =>
+    arr.length === amount
+        ? arr
+        : newGrade(amount, [
+              ...arr,
+              Grade(
+                  grades.subject[randint(0, grades.subject.length)],
+                  randint(1, 10),
+                  grades.ETC[randint(0, grades.ETC.length)],
+                  newProfessors(randint(1, 2))
+              ),
+          ])
 
-const professors1 = (): Array<Professor> => [
-    Professor('John', 44, 'john@hr.nl', schools2()),
-    Professor('Yuno', 28, 'yuno@hr.nl', schools1()),
-]
+const newProfessors = (amount: number, arr: Array<Professor> = []): Array<Professor> =>
+    arr.length === amount ? arr : newProfessors(amount, [...arr, newProfessor()])
 
-const professors2 = (): Array<Professor> => [Professor('Hubert', 32, 'hubert@hr.nl', schools1())]
+const schools = {
+    name: [
+        'Hogeschool Rotterdam',
+        'Avans Hogeschool',
+        'Universiteit van Amsterdam',
+        'TU Delft',
+        'Universiteit Utrecht',
+        'Inholland',
+        'LOI',
+        'Haagse Hogeschool',
+        'Has Hogeschool',
+        'HAN University of Applied Sciences',
+        'Hogeschool Utrecht',
+        'Erasmus University',
+        'Hogeschool Leiden',
+        'Codarts Rotterdam',
+    ],
+    location: () => `${address.streetName()}, ${address.cityName()}`,
+    founded: () => randint(1960, 2020),
+}
 
-const professors3 = (): Array<Professor> => [Professor('Michael', 33, 'michael@hr.nl', schools3())]
+const grades = {
+    subject: [
+        'Development 1',
+        'Development 2',
+        'Development 3',
+        'Development 4',
+        'Development 5',
+        'Development 6',
+        'Development 7',
+        'Development 8',
+        'Business Basic',
+        'Business Expert',
+        'Art',
+        'Medicine',
+        'Singing',
+        'Marketing',
+    ],
+    ETC: [1, 2, 10, 20, 29, 30],
+}
 
-const schools1 = (): Array<School> => [
-    School('Hogeschool Rotterdam', 'Wijnhaven', 1960),
-    School('Hogeschool Rotterdam', 'Academieplein', 1971),
-]
-
-const schools2 = (): Array<School> => [
-    School('Hogeschool Rotterdam', 'Kralingse Zoom', 2001),
-    School('Hogeschool Rotterdam', 'Academieplein', 1999),
-]
-
-const schools3 = (): Array<School> => [School('Hogeschool Rotterdam', 'Museumpark', 2000)]
+export const initialize = (amount: number, arr: Array<Student> = []): Array<Student> =>
+    arr.length === amount ? arr : initialize(amount, [...arr, newStudent()])
--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,49 @@ import { Unit } from './lib/unit'
 const main = () => {
     console.log('Main program \n')
 
-    // ! SETUP
-    const data = State<Student, Unit>(initialize())
+    const data = State<Student, Unit>(initialize(100))
     const students = Query(data)
+    const lazyStudents = LazyQuery<Student>(Fun(Id))
 
-    const query1 = students.select('firstName', 'age').groupBy('age').toList()
+    // ! 1. SELECT
+    const query1 = students.select('firstName', 'lastName').select('email').select('age')
 
-    prettyPrint('Query1', query1)
+    const lzquery1 = lazyStudents.select('firstName', 'lastName').select('email').select('age')
+
+    // prettyPrint('Query1', query1.toList())
+    // prettyPrint('Query1 lazy', lzquery1.toList(students))
+
+    // ! 2. INCLUDE
+    const query2 = query1.include('Grades', (q) => q.select('name', 'grade'))
+
+    const lzquery2 = lzquery1.include('Grades', (q) => q.select('name', 'grade'))
+
+    // prettyPrint('Query2', query2.toList())
+    // prettyPrint('Query2 Lazy', lzquery2.toList(students))
+
+    // ! 3. WHERE
+    const query3 = query2.where((x) => x.get('age').between(25, 30))
+
+    const lzquery3 = lzquery2.where((x) => x.get('age').between(25, 30))
+
+    // prettyPrint('Query3', query3.toList())
+    // prettyPrint('Query3 Lazy', lzquery3.toList(students))
+
+    // ! 4. ORDER BY
+    const query4 = query3.orderBy('age', 'ASC')
+
+    const lzquery4 = lzquery3.orderBy('age', 'ASC')
+
+    // prettyPrint('Query4', query4.toList())
+    // prettyPrint('Query4 Lazy', lzquery4.toList(students))
+
+    // ! 5. GROUP BY
+    const query5 = query4.groupBy('age')
+
+    // const lzquery5 = lzquery4
+
+    prettyPrint('Query4', query5.toList())
+    // prettyPrint('Query4 Lazy', lzquery4.toList(students))
 }
 
 main()
